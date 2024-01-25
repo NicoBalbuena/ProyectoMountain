47ef658749c9cd71876a52a2074f8520b2cfc828
Santiago Delebecq <santiidele21@gmail.com>
2024-01-25 01:13:59 -0300

    add cloudinary to post places and upload

--- Airbnb-clone/server/middleware/cloudinary-middleware.js
+++ Airbnb-clone/server/middleware/cloudinary-middleware.js
@@ -2,12 +2,27 @@ const cloudinary = require("../utils/cloudinary-config");
 
 async function cloudinaryMiddleware(req, res, next) {
   try {
-    if (!req.body.data.photos) {
+    let photos;
+
+    // Verificar si las fotos están en el cuerpo como JSON
+    if (req.body.data && req.body.data.photos) {
+      photos = req.body.data.photos;
+    }
+
+    // Si no se encontraron fotos en formato JSON, verificar si están en formato form
+    if (!photos && req.files && req.files.length > 0) {
+      // Obtener las URLs de las fotos desde los archivos subidos
+      photos = req.files.map((file) => file.path);
+    }
+
+    // Si no hay fotos, devolver un error
+    if (!photos || photos.length === 0) {
       return res.status(404).json({ Message: "Image not found" });
     }
 
-    const uploadPromises = req.body.data.photos.map(async (photoUrl) => {
-      const result = await cloudinary.uploader.upload(photoUrl, {
+    // Subir las fotos a Cloudinary
+    const uploadPromises = photos.map(async (photo) => {
+      const result = await cloudinary.uploader.upload(photo, {
         upload_preset: "ml_default",
         allowed_formats: [
           "png",
@@ -26,7 +41,13 @@ async function cloudinaryMiddleware(req, res, next) {
 
     const uploadedImages = await Promise.all(uploadPromises);
 
-    req.body.data.photos = uploadedImages;
+    // Actualizar las fotos en el cuerpo de la solicitud
+    if (req.body.data) {
+      req.body.data.photos = uploadedImages;
+    } else {
+      req.body.photos = uploadedImages;
+    }
+
     next();
   } catch (error) {
     console.error(error);
