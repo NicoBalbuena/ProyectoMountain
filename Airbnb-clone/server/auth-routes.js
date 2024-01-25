684b43db21ca152ff84b090203a657f3360e3882
Tivani381 <t.ivani381@gmail.com>
2024-01-25 16:48:07 -0300

    cambio auth routes

--- Airbnb-clone/server/auth-routes.js
+++ Airbnb-clone/server/auth-routes.js
@@ -16,7 +16,7 @@ authRouter.get('/login/google/callback', passport.authenticate('google', { failu
   (req, res) => {
     console.log('Perfil del usuario autenticado:', req.user);
     // Redirección exitosa después de la autenticación con Google
-    res.redirect('http://localhost:5173/');
+    res.send(req.user.accessToken);
   }
 );
 
