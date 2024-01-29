cdb09a2ccecf8aae09a0f9b9b49a10528e1382c4
Santiago Delebecq <santiidele21@gmail.com>
2024-01-29 02:34:16 -0300

    Dashboard: add rechart graphic

--- Airbnb-clone/client/src/components/transactionChart.jsx
+++ Airbnb-clone/client/src/components/transactionChart.jsx
@@ -6,9 +6,10 @@ import {
   ResponsiveContainer,
   Tooltip,
   XAxis,
+  YAxis,
 } from "recharts";
 
-const dataUser = [
+const dataUsers = [
   { month: "Enero", users: 150 },
   { month: "Febrero", users: 200 },
   { month: "Marzo", users: 180 },
@@ -23,20 +24,22 @@ const dataUser = [
   { month: "Diciembre", users: 280 },
 ];
 
+
 export const TransactionChart = () => {
   return (
-    <div className="h-[22rem] bg-white p-4 rounded-sm border-x-gray-200 flex-col flex-1">
+    <div className="h-[22rem] bg-white p-4 rounded-sm border border-x-gray-200 flex flex-col ">
       <h2 className="text-pink-500 font-medium text-lg">Users</h2>
-      <div className="w-full mt-3 flex-1 text-xs">
-        <ResponsiveContainer width={"100%"} height={"100%"}>
-        <BarChart
+      <div className="w-full  mt-3 flex-1 text-xs">
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
             width={500}
             height={300}
-            data={dataUser}
+            data={dataUsers}
             margin={{ top: 20, right: 10, left: -10, bottom: 0 }}
           >
             <CartesianGrid strokeDasharray="3 3 0 0" vertical={false} />
             <XAxis dataKey="month" />
+            <YAxis/>
             <Tooltip />
             <Legend />
             <Bar dataKey="users" fill="#0ea5e9" />
