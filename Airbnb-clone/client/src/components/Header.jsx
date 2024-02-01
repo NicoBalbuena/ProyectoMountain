7df89a524d437f21eca363c4b6c584ed3e28a446
angeltena <angeltena2742@gmail.com>
2024-02-01 03:31:19 -0600

    diseños nuevos

--- Airbnb-clone/client/src/components/Header.jsx
+++ Airbnb-clone/client/src/components/Header.jsx
@@ -1,48 +1,41 @@
 import { useContext } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { UserContext } from "./UserContext";
+import { Tipi } from "@phosphor-icons/react";
 
 
 const Header = () => {
-
-    const { pathname } = useLocation()
 
     const { user } = useContext(UserContext)
 
     return (
         <div>
+            
+            <header className="flex justify-between py-3 px-5">
+                <Link to={"/"} className="flex items-center gap-1">
+                <Tipi size={32} weight="thin" />
 
-        <header className="flex justify-between py-3 px-5">
-            <Link to={"/"} className="flex items-center gap-1">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                </svg>
+                    <span className="font-semibold text-lg">MountainHaven</span>
+                </Link>
 
-                <span className="font-bold text-xl">MountainHaven</span>
-            </Link>
-
-            <Link to={pathname !== `/about` ? "/about" : `/`} className="flex items-center gap-2 border bg-primary border-gray-300 rounded-full py-2 px-8 hover:bg-yellow-300 transition ">
-                About
-            </Link>
-
-            <Link to={user ? "/account" : "/login"} className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 ">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                </svg>
-                <div className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 relative top-1">
-                        <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
+                <Link to={user ? "/account" : "/login"} className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 ">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
-                </div>
-                {!!user && (
-                    <div>
-                        {user.name}
+                    <div className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 relative top-1">
+                            <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
+                        </svg>
                     </div>
-                )}
-            </Link>
-        
-        </header>
-        <hr />
+                    {!!user && (
+                        <div>
+                            {user.name}
+                        </div>
+                    )}
+                </Link>
+
+            </header>
+            <hr />
         </div>
 
     )
